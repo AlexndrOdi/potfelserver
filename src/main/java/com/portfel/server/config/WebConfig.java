0071e523934067eef1b8e2c18af38186dca5117f
AlexndrOdi <36817201+AlexndrOdi@users.noreply.github.com>
2018-03-07 18:28:43 +0300

    fix view resolver (now working html and js) update MessageController and WebConfig add security dependencies

--- src/main/java/com/portfel/server/config/WebConfig.java
+++ src/main/java/com/portfel/server/config/WebConfig.java
@@ -9,11 +9,13 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.script.ScriptTemplateViewResolver;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,6 +25,11 @@ import java.util.Properties;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.portfel.server")
 public class WebConfig extends WebMvcConfigurerAdapter{
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
 
@@ -31,5 +38,20 @@ public class WebConfig extends WebMvcConfigurerAdapter{
         converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
 
         converters.add(converter);
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setOrder(0);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".html");
+
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("//**").addResourceLocations("/");
     }
 }
--- src/main/java/com/portfel/server/controller/MessageController.java
+++ src/main/java/com/portfel/server/controller/MessageController.java
@@ -4,38 +4,37 @@ import com.portfel.server.entity.Message;
 import com.portfel.server.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(value = "/welcome")
+@Controller
+@RequestMapping(value = "/messages")
 public class MessageController {
 
     @Autowired
     private MessageService messageService;
 
-    @RequestMapping(value = "/messages", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     @ResponseBody
-    public List<Message> getAllMessages(){
+    public List<Message> getAllMessages()
+    {
         return messageService.getAll();
     }
 
-    @RequestMapping(value = "/message{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public Message getMessageById(@PathVariable long id){
         return messageService.getById(id);
     }
 
-    @RequestMapping(value = "/addmessages", method = RequestMethod.POST)
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseBody
     public Message saveMessage(@RequestBody Message message){
         return messageService.save(message);
     }
 
-    @RequestMapping(value = "/messages{id}", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     @ResponseBody
     public void deleteMessage (@PathVariable long id){
         messageService.delete(id);
